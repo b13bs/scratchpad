e366e4c3253c3f24507e2119bcaf8e893e6e8ee9
b13bs <etienne.ducharme@gmail.com>
2018-08-06 15:19:42 -0400

    mo4r logging function

--- bash-port-scanner.sh
+++ bash-port-scanner.sh
@@ -2,6 +2,14 @@
 
 ipaddr="XX.XX.XX.XX"
 
-for port in {1..65535}; do
-    (echo  > /dev/tcp/$ipaddr/$port) >& /dev/null && echo "Port $port open"
+logfilename="$HOME/$(date '+%Y-%m-%d_%H-%M-%S')-portscan.log"
+
+function log_success ()
+{
+    string="Port $1 open"
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $string" >> $logfilename
+}
+
+for port in {1..1000}; do
+    (echo  > /dev/tcp/$ipaddr/$port) >& /dev/null && log_success $port || echo "Port $port close"
 done
