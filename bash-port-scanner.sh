a187ae4620808eb99208dd178fdff7cdaf7e0745
b13bs <etienne.ducharme@gmail.com>
2018-08-06 16:30:12 -0400

    port scan

--- bash-port-scanner.sh
+++ bash-port-scanner.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
 ipaddr="XX.XX.XX.XX"
+logfilename="$HOME/portscan-$(date '+%Y-%m-%d_%H-%M-%S').log"
 
-logfilename="$HOME/$(date '+%Y-%m-%d_%H-%M-%S')-portscan.log"
+
+trap ctrl_c INT
+function ctrl_c() {
+    echo "** Trapped CTRL-C"
+    exit
+}
 
 function log_success ()
 {
-    string="Port $1 open"
+    string="port is $1 opened"
     echo "$(date '+%Y-%m-%d %H:%M:%S') - $string" >> $logfilename
 }
 
-for port in {1..1000}; do
-    (echo  > /dev/tcp/$ipaddr/$port) >& /dev/null && log_success $port || echo "Port $port close"
+
+for port in {1..1001}; do
+    timeout 1 bash -c "echo > /dev/tcp/$ipaddr/$port &> /dev/null" &&
+        log_success $port ||
+        echo "port $port is closed"
 done
